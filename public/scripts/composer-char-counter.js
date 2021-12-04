d36c27d2955160f4be97315f95028e9307a17753
pochiman <pochiman@yahoo.com>
2021-12-04 13:36:38 -0500

    switch the counter and its color based on the text field's reach

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+// Function which manipulates the char counter in the create new tweet element
+
+$(document).ready(function() {
+  
+  $('textarea').on('input', function() {
+    const maxCount = 140;
+    const inputLength = $(this).val().length;
+    $(this).nextAll(".counter").text(maxCount - inputLength);
+    if (maxCount - inputLength < 0) {
+      $(this).nextAll(".counter").css("color", "red");
+    } else {
+      $(this).nextAll(".counter").css("color", "#545149");
+    }
+  });
+  
+});
